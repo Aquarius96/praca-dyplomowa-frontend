813f98624f2d20bf25c2ec30df74ed7fd098fc5e
Marcin Zapadka <marcinzapadka33@wp.pl>
2018-08-10 19:47:54 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Praca dyplomowa frontend
-      </div>
+        <Routes />        
+      </div> 
     );
   }
 }
