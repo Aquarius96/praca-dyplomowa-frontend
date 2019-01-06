f8dbb90c482df90d574d6ec3c7838877064e7cd7
Marcin Zapadka <marcinzapadka33@wp.pl>
2019-01-06 16:39:11 +0100

    Fix cors

--- src/utils/axios-config.js
+++ src/utils/axios-config.js
@@ -1,6 +1,7 @@
 export const URL = "https://bazaksiazek.azurewebsites.net/api/";
 export const headers = {
   headers: {
+    'Access-Control-Allow-Origin': 'https://bazaksiazek.azurewebsites.net',
     Authorization: "Bearer " + localStorage.getItem("token")
   }
 };
